e3644da33b18014844109bf6d3e4227336aec5a7
Joshua Smith <joshua-smith@verizon.net>
2013-04-24 14:12:34 -0300

    Don't be parse-nickity!

---
+++ resources/odml/parse.js
@@ -0,0 +1,11 @@
+// This JS-based parser was made by the FireText/ODML contributors.
+
+var odml = {};
+
+odml.parse = function (data, outputType) {
+  // Some code to convert ODML into something else
+};
+
+odml.encode = function (data, type) {
+  // Some code to convert data to ODML
+};
