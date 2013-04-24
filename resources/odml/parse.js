9010cc08f62dd12be3180a37968641a709a6f847
Logan <kai.ralston@gmail.com>
2013-04-24 16:12:16 -0300

    Add encode from HTML to ODML

--- resources/odml/parse.js
+++ resources/odml/parse.js
@@ -8,4 +8,21 @@ odml.parse = function (data, outputType) {
 
 odml.encode = function (data, type) {
   // Some code to convert data to ODML
+  output = "";
+  if (type == "HTML") {
+    // Start document
+    output += "<!DOCTYPE odml><odml>";
+    // Add info
+    output += "<info><title>TODO</title></info>";
+    // Add content
+    output += "<content>";
+    output += data;
+    output += "</content>";
+    // End document
+    output += "</odml>";
+    // Return document
+    return output;
+  }
+  // Didn't convert to ODML, return false
+  return false;
 };
