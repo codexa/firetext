dff094dcea2327f6a9fa909a7cdda69791661fe9
Logan <kai.ralston@gmail.com>
2013-05-03 14:50:11 -0300

    Fix #37

--- resources/txt/parse.js
+++ resources/txt/parse.js
@@ -6,11 +6,11 @@ txt.parse = function (data, type) {
   // Some code to convert TXT into something else
   output = "";
   if (type == "HTML") {
-    output = data.replace(/<br\/>/gi, '\n');
-    output = output.replace(/<br>/gi, '\n');
     var tmp = document.createElement("DIV");
     tmp.innerHTML = output;
-    return tmp.textContent;
+    output =  "</pre>" +tmp.textContent + "</pre>";
+    output = data.replace(/\n/gi, '<br/>');
+    return output;
   }
   // Didn't parse, return false
   return false;
@@ -20,7 +20,8 @@ txt.encode = function (data, type) {
   // Some code to convert data to TXT
   output = "";
   if (type == "HTML") {
-    output = data.replace(/\n/gi, '<br/>');
+    output = data.replace(/<br\/>/gi, '\n');
+    output = output.replace(/<br>/gi, '\n');
     return output;
   }
   // Didn't convert to TXT, return false
