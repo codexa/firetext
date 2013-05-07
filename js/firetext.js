4f9d79977471ef6ba2404c62bed57d14b1ef3e8b
Joshua Smith <joshua-smith@verizon.net>
2013-05-07 15:49:55 -0400

    Improve first run

--- js/firetext.js
+++ js/firetext.js
@@ -263,9 +263,11 @@ function docsInFolder(callback) {
   
   cursor.onerror = function() {
     if (cursor.error.name == 'TypeMismatchError') {
-      saveFile('Welcome','.html','<b>Welcome</b> to <i>Firetext</i>!', false, function() {
-        loadToEditor('Welcome', '.html');
+      saveFile('firetext','.temp','A temp file!  You should not be seeing this.  If you see it, please report it to <a href="https://github.com/codexa/firetext/issues/" target="_blank">us</a>.', false, function() {
       });
+      updateDocLists();
+      deleteFile('firetext.temp');
+      return;
     } else if (cursor.error.name == 'SecurityError') {
       alert('Please allow Firetext to access your SD card.');
     } else {
