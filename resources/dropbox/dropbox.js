bba40067f602a5685ca7a8502158a6b11e81498e
Joshua Smith <joshua-smith@verizon.net>
2013-06-08 20:01:04 -0400

    Revert "Revert "Revert "Merge branch '0.2' of https://github.com/Codexa/Firetext into 0.2"""
    This reverts commit 0907330d6f1ebd503810ecb8674324b36de9b899.

--- resources/dropbox/dropbox.js
+++ resources/dropbox/dropbox.js
@@ -1,10 +1,8 @@
 var dropAPI = {};
 
 dropAPI.client = new Dropbox.Client({
-  key: "CBB0GYTWGYA=|aeSB7VBcIP94mzfQPoykIzGm++Z97KtaDn2snjXCGQ=="
+    key: "CBB0GYTWGYA=|aeSB7VBcIP94mzfQPoykIzGm++Z97KtaDn2snjXCGQ=="
 });
 
 dropAPI.client.authDriver(new Dropbox.Drivers.Popup({
-  rememberUser: true,
-  receiverUrl: "http://codexa.co.nf/firetext/dropbox-success/"
-}));
+    receiverUrl: "http://firetext.co.nf/dropbox-thanks"}));
