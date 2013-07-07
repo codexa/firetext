4836f90337f3df762d0279402245ad0f1ed0bbf1
Joshua Smith <joshua-smith@verizon.net>
2013-07-06 20:40:46 -0400

    Fix #99!

--- resources/txt/parse.js
+++ resources/txt/parse.js
@@ -6,10 +6,10 @@ txt.parse = function (data, type) {
   // Some code to convert TXT into something else
   output = "";
   if (type == "HTML") {
-    var tmp = document.createElement("DIV");
-    tmp.innerHTML = output;
-    output = tmp.textContent;
-    output = data.replace(/\n/gi, '<br/>');
+    output = data.replace(/</gi, '<pre><code>&lt;')
+    			 .replace(/>/gi, '&gt;</code></pre>')
+    			 .replace(/&amp;/gi, '&amp;amp;')
+    			 .replace(/\n/gi, '<br>');
     return output;
   }
   // Didn't parse, return false
@@ -20,8 +20,8 @@ txt.encode = function (data, type) {
   // Some code to convert data to TXT
   output = "";
   if (type == "HTML") {
-    output = data.replace(/<br\/>/gi, '\n');
-    output = output.replace(/<br>/gi, '\n');
+    output = data.replace(/<br\/>/gi, '\n')
+    			 .replace(/<br>/gi, '\n');
     var tmp = document.createElement("DIV");
     tmp.innerHTML = output;
     return tmp.textContent;
