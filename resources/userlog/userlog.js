15eab849995052e6f087a7de9e5f84758158dc58
hrandev <hrpearl01@gmail.com>
2013-08-25 13:22:58 +0100

    fix errors, add log message

--- resources/userlog/userlog.js
+++ resources/userlog/userlog.js
@@ -6,37 +6,49 @@
 /*
 Log massages
 ================*/
-// [date&time] clientid {document name}? {document extension}? {message} {location(cloud|internal)}? {feature}?
+// [DD/MM/YY SEC:MIN:HOURS] clientid {document name}? {document extension}? {action} {location(cloud|internal)}? {feature}?
+var ln = " logged ", dn = " document ", en = " enabled ", din = " disabled ", cn = " changed to ", on = " opened ", nightmodeSelect = getSettings('nightmode');
+
 firetext.user.log = {
-	mn: { ln : " logged ", dn : " document ", en : " enabled ", din : " disabled ", cn : " changed to ", on : " opened "},
 	m: {
 	    log: {
-	        lin: mn.ln + "in from ",
-	        out: mn.ln + "out from "
+	        lin: ln + "in from ",
+	        out: ln + "out from "
 	    },
 	    doc: {
-	        open: mn.dn + "opened at ",
-	        close: mn.dn + "closed at ",
-	        create: mn.dn + "created at ",
-	        ddelete: mn.dn + "deleted at "
+	        open: dn + "opened at ",
+	        close: dn + "closed at ",
+	        create: dn + "created at ",
+	        ddelete: dn + "deleted at "
 	    },
 	    enable: {
-	        autosave: mn.en + "autosave",
-	        lastfile: mn.en + "load last file on start up",
+	        autosave: en + "autosave",
+	        lastfile: en + "load last file on start up",
 	        zen: en + "zen mode"
 	    },
 	    disable: {
-	        autosave: mn.din + "autosave",
-	        lastfile: mn.din + "load last file on start up",
+	        autosave: din + "autosave",
+	        lastfile: din + "load last file on start up",
 	        zen: din + "zen mode"
 	    },
 	    change: {
-	        nval: nightmodeSelect.value,
-	        night: " night mode" + mn.cn + this.nval
+	        night: " night mode" + cn + nightmodeSelect.value
 	    },
 	    open: {
-	        about: mn.on + "about",
-	        ftsup: mn.on + "Firetext support"
+	        about: on + "about",
+	        ftsup: on + "Firetext support"
 	    }
 	}
+};
+
+function logm(docname, docext, act, feature, loc){
+	var d = new Date();
+	// DD/MM/YY 00:00:00
+	this.date = d.getUTCDate() + "/" + (d.getUTCMonth()+1) + "/" + d.getUTCFullYear().toString().substr(2,2);
+    this.time = d.getUTCSeconds() + ":" + d.getUTCMinutes() + ":" + d.getUTCHours();
+    this.datime = "[" + date + " " + time + "]";
+    this.clid = "";// get client ID
+	this.docn = docname;
+	this.doce = docext;
+	this.act = firetext.user.log.m[action];
 }
