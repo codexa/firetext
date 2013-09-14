bad9c5deb2cf931fa4cd2a5bee68433b6bc5f456
hrandev <hrpearl01@gmail.com>
2013-09-14 23:02:52 +0100

    Fix #123

--- scripts/user/id.js
+++ scripts/user/id.js
@@ -11,19 +11,18 @@
 // Namespace
 firetext.user.id = {};
 
-
 /* User ID
 ------------------------*/
 firetext.user.id.init = function () {
-  var ClId = firetext.user.$_ClientID,
+  var ClId = firetext.user.id._$Cl,
       CClId = window.localStorage.getItem("$#ClId");
   // Client ID Verification and Validation
   if (ClId === undefined && CClId === null){
     firetext.user.id.generate();
   } else if(ClId === undefined){
-    firetext.user.$_ClientID = localStorage.getItem("$#ClId");
+    firetext.user.id._$Cl = localStorage.getItem("$#ClId");
   } else if (CClId === null){
-    localStorage.setItem("$#ClId", firetext.user.$_ClientID);
+    localStorage.setItem("$#ClId", firetext.user.id._$Cl);
   } else if(ClId.length/4 !== 16 || CClId.length/4 !== 16){
     firetext.user.id.generate();
     firetext.user.id.init();
@@ -33,26 +32,26 @@ firetext.user.id.init = function () {
 firetext.user.id.generate = function () {
   var edi = "",
       ch1 = "",
-	  nm1 = "",
-	  nm2 = "",
-	  sb1 = "",
-	  sb2 = "",
-	  sb3 = "",
-	  Cep = [],
-	  Cij = [],
-	  ClID = "",
-	  chnm = [],
-	  chi = function (l) {
-	    var arg = arguments[1], a = 0, chp;
-		  if (arg !== undefined){
-		    a = arg;
-		    chp = chnm[a].charAt(l);
-		  } else {
-		    chp = chnm[a].charAt(chnm[a].indexOf("D") + (l));
-		  }
-		  return chp;
-	    };
-	    
+  	  nm1 = "",
+  	  nm2 = "",
+  	  sb1 = "",
+  	  sb2 = "",
+  	  sb3 = "",
+  	  Cep = [],
+  	  Cij = [],
+  	  ClID = "",
+  	  chnm = [],
+  	  chi = function (l) {
+  	    var arg = arguments[1], a = 0, chp;
+  		  if (arg !== undefined){
+  		    a = arg;
+  		    chp = chnm[a].charAt(l);
+  		  } else {
+  		    chp = chnm[a].charAt(chnm[a].indexOf("D") + (l));
+  		  }
+  		  return chp;
+  	    };
+
   chnm = ["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", "0123456789", "#?!_", "&%", "$!£"];
   edi = chi(-1)+chi(0)+chi(2)+chi(42)+chi(-1);
   
@@ -71,9 +70,9 @@ firetext.user.id.generate = function () {
   
   Cep = [chi(3,2)+chi(0,2)+chi(0,4)+chi(26)+chi(4)+chi(8-5,1)+chi(9-5,1)+chi(21)+chi(23)+chi(3)+chi(1,3)+chi(0,4)+chi(2,4)+chi(5-5,1)+chi(14-5,1)+chi(5,5-5,1)].join();
   Cij = [edi + sb1 + sb2 + ch1 + nm1 + chnm[2].charAt(0) + nm2 + sb3].join();
-  ClID = CryptoJS.AES.encrypt(Cij, Cep).toString; // 64-bit number
+  ClID = CryptoJS.AES.encrypt(Cij, Cep).toString(); // 64-bit number
   window.localStorage.setItem("$#ClId", ClID);
-  firetext.user.$_ClientID == ClID;
+  window.firetext.user.id._$Cl == ClID;
   
   return ClID;
 };
