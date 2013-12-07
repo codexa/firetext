56298b3410b3b85df68336e7db5462f7e34ef694
Nick Wong <hrpearl01@gmail.com>
2013-12-07 15:09:08 +0000

    fireanalytics object

--- scripts/analytics.js
+++ scripts/analytics.js
@@ -1,35 +1,53 @@
 /*
-* Recent Docs
-* Copyright (C) Codexa Organization 2013.
-*/
+ * Recent Docs
+ * Copyright (C) Codexa Organization 2013.
+ */
 
 'use strict';
 
 
 /* Namespace Container
-------------------------*/ 
+------------------------*/
 firetext.analytics = {};
 firetext.analytics.doc = {
-    name:,
-    modifyDate:,
-    size:,
-    words:,
-    characters: 
+    name: ,
+    path: ,
+    size: ,
+    modifyDate: ,
+    words: ,
+    characters:
 };
 
 
 /* Recent Docs
 ------------------------*/
 // Initalize doc analytics
-firetext.analytics.doc.init = function () {
-  firetext.analytics.doc.findWords = function(text){
+firetext.analytics.doc.init = function() {
+    var FtanlCD = window.localStorage.getItem("FtanlCD");
+    if (FtanlCD) {
+
+    } else if (firetext.analytics.doc.name) {
+        window.localStorage.setItem("FtanlCD", JSON.stringify(firetext.analytics.doc);
+    }
+};
+firetext.analytics.doc.findWords = function(text) {
     var count,
         temp;
     // Sanitize
-    text = text.replace(/\W+/g, " ");
-    text = text.split(" ");
-    count = text.length;
+    temp = text.replace(/\W+/g, " ");
+    temp = temp.split(" ");
+    count = temp.length;
     firetext.analytics.doc.words = count;
-  }
+}
+firetext.analytics.doc.findChars = function(text) {
+    var count,
+        temp;
+    // Sanitize
+    temp = text.split("");
+    count = temp.length;
+    firetext.analytics.doc.characters = count;
+}
+firetext.analytics.doc.modified = function() {
 
-};
+    firetext.analytics.doc.modifyDate = ;
+}
