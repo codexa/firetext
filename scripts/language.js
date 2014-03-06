5504283e7c4ffdd83fe3f8a92fd80240beea6724
Joshua Smith <joshua-smith@verizon.net>
2014-03-06 08:57:46 -0500

    Fix #165, Add RTL spinner, small graphical changes

--- scripts/language.js
+++ scripts/language.js
@@ -15,16 +15,27 @@ firetext.language = function () { return this.getCurrent(); };
 /* Init
 ------------------------*/ 
 firetext.language.init = function () {
-  if (!firetext.settings.get('language')) {
-    firetext.settings.save('language', document.webL10n.getLanguage());    
-  } 
-  
-  // Localize interface
-  var language = firetext.settings.get('language');
-  document.webL10n.setLanguage(language);
-  document.body.setAttribute('language', language);
+	if (!firetext.settings.get('language')) {
+		firetext.settings.save('language', document.webL10n.getLanguage());		 
+	} 
+	
+	// Localize interface
+	var language = firetext.settings.get('language');
+	document.webL10n.setLanguage(language);
+	document.body.setAttribute('data-language', language);
+	
+	// LTR / RTL
+	if (language == 'he' |
+			language == 'ar' |
+			language == 'tr') {
+		document.body.classList.remove('ltr');  
+		document.body.classList.add('rtl');  		
+	} else {
+		document.body.classList.remove('rtl');  
+		document.body.classList.add('ltr');  
+	}
 }
 
 firetext.language.getCurrent = function () {
-  return (firetext.settings.get('language'));
+	return (firetext.settings.get('language'));
 }
