382d57a0f142128856d69903306b01a3d8e27036
joshua-s <thewanuki@gmail.com>
2015-02-05 03:32:19 +0000

    Add server files

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+if (isset($_POST) && !empty($_POST)) {
+	if($_POST['request']=='urls') {
+		// Create variables
+		$urls = array(
+					"about"=>"",
+					"credits"=>"",
+					"support"=>"",
+					"rate"=>"https://marketplace.firefox.com/app/firetext/ratings/add",
+					"dropboxAuth"=>"http://firetext.codexa.bugs3.com/auth/success/dropbox/"
+				);
+	
+		// Echo variables
+		header('Content-Type: application/json');
+		header('Access-Control-Allow-Origin: *');
+		echo json_encode($urls);
+		exit;
+	}
+}
