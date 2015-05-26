45fbd34af5a920da358934764018d458b76f9461
Joshua Smith <thewanuki@gmail.com>
2015-05-26 18:53:50 -0400

    Update server to dropbox.js 0.10.3

--- index.php
+++ index.php
@@ -2,13 +2,22 @@
 
 if (isset($_POST) && !empty($_POST)) {
 	if($_POST['request']=='urls') {
+		// Use Dropbox OAuth2 for recent versions
+		if (version_compare($_POST['version'], 0.4, '>')) {
+			$dropboxURL = 'https://codexa.github.io/firetext/auth/dropbox/oauth2/';
+			$dropboxKey = 'p8qpg4ai84mfayx';
+		} else {
+			$dropboxURL = 'https://codexa.github.io/firetext/auth/dropbox/';
+		}
+		
 		// Create variables
 		$urls = array(
 					"about"=>"",
 					"credits"=>"",
 					"support"=>"",
 					"rate"=>"https://marketplace.firefox.com/app/firetext/ratings/add",
-					"dropboxAuth"=>"https://codexa.github.io/firetext/auth/dropbox/"
+					"dropboxAuth"=>$dropboxURL,
+					"dropboxKey"=>$dropboxKey
 				);
 	
 		// Echo variables
